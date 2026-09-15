e7c77b1222644ed916d5c25bc4e5f3ed4cf776c8
Anoop Kushwaha <anoopit41@gmail.com>
2026-09-15 18:29:01 +0530

    chore: update cerrypick content.

--- cerryPick.py
+++ cerryPick.py
@@ -4,3 +4,10 @@ try:
 except TypeError:
     print("Enter a valid input")
 
+
+
+stu = ["Ajeet","Rahul","Harshit"]
+try:
+    result = stu[len(stu)]
+except IndexError:
+    print("Index out bound.")
